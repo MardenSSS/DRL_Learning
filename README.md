48fbaa55827947625e27b91181cd12e0f131c065
marden <969626066@qq.com>
2023-11-02 19:52:49 +0800

    dqn algorithm

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# DRL_Learning
+# Description
+
+DRL_Learning: A learning framework of Deep Reinforcement Learning
+
+# Envirinment
+
+<ul>
+<li>cuda = 11.7</li>
+<li>python = 3.8.18</li>
+<li>pytorch = 1.13.1</li>
+<li>gym = 0.25.2</li>
+</ul>
+
+### Tips1:
+
+<ul>
+<li> AttributeError:‘CartPoleEnv‘ object has no attribute ‘seed‘</li>
+<li> gym版本过高，降低gym版本</li>
+<li>pip uninstall gym</li>
+<li>pip install gym==0.25.2</li>
+</ul>
---
+++ utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+# 移动平均算法：常用的时序数据平滑方法，通过计算一段时间窗口内的数据平均值来减少噪音并突出数据的主要趋势
+def moving_average(a, window_size):
+    cumulative_sum = np.cumsum(np.insert(a, 0, 0))
+    middle = (cumulative_sum[window_size:] - cumulative_sum[:-window_size]) / window_size
+    r = np.arange(1, window_size - 1, 2)
+    begin = np.cumsum(a[:window_size - 1])[::2] / r
+    end = (np.cumsum(a[:-window_size:-1])[::2] / r)[::-1]
+    return np.concatenate((begin, middle, end))
